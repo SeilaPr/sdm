2091e52e3bd5ba2289bcc38b66a627bcef155c27
robertoperez <robertops1818@gmail.com>
2018-01-07 15:24:22 +0100

    Añadido el feed de twitter
    Parte de Teji pero no le tiraba el android

--- app/src/main/java/com/example/seila/gigiaradio/MainActivity.java
+++ app/src/main/java/com/example/seila/gigiaradio/MainActivity.java
@@ -5,8 +5,18 @@ import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter;
+import com.twitter.sdk.android.tweetui.UserTimeline;
 
 import java.io.IOException;
 
@@ -23,6 +33,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         play = (Button) findViewById(R.id.btnPlay);
+        twitter();
+    }
+
+    private void twitter() {
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("1zkRSru1gH4Ou6RNbRN75mw5o", "hRQy0pjf7D0LIAybkGqMC0BcuFTVzzIUQkCeGEqCqRY7wvOtN7"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycled);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final UserTimeline userTimeline = new UserTimeline.Builder().screenName("gigiaradio").build();
+
+        final TweetTimelineRecyclerViewAdapter adapter =
+                new TweetTimelineRecyclerViewAdapter.Builder(this)
+                        .setTimeline(userTimeline)
+                        .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+                        .build();
+
+        recyclerView.setAdapter(adapter);
     }
 
     public void setPlaying(boolean playing)
