fcd8301647ff20dbf28a5236051294ff3a7b5003
Roberto <robertops1818@gmail.com>
2018-01-09 14:52:40 +0100

    Añadido email

--- app/src/main/java/com/example/seila/gigiaradio/ContactoActivity.java
+++ app/src/main/java/com/example/seila/gigiaradio/ContactoActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ContactoActivity extends AppCompatActivity {
 
@@ -33,5 +34,25 @@ public class ContactoActivity extends AppCompatActivity {
         Uri uri = Uri.parse("https://www.instagram.com/gigiaradio");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
+    }
+
+    public void sendEmail(View view) {
+        String[] TO = {"gigiaradioceo@gmail.com"}; //aquí pon tu correo
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Escribe aquí tu mensaje");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+            finish();
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ContactoActivity.this,
+                    "No tienes clientes de email instalados.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
